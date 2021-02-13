854b39a8f2b0f2c18368d5c17415ea6eb72b04db
paraspatni25 <paras.d.patni@gmail.com>
2021-02-13 09:40:46 -0500

    updated MainActivity.java

--- app/src/main/java/com/example/vacination_app/MainActivity.java
+++ app/src/main/java/com/example/vacination_app/MainActivity.java
@@ -4,34 +4,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button joinNowButton, loginButton;
+    TextView loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        joinNowButton = (Button) findViewById(R.id.sign_up_button);
-        loginButton = (Button) findViewById(R.id.log_in_text_button);
+        Button signUp_button = (Button) findViewById(R.id.sign_up_button);
+        loginButton = (TextView) findViewById(R.id.log_in_text_button);
+
+        signUp_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        joinNowButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, SignupActivity.class);
                 startActivity(intent);
             }
         });
