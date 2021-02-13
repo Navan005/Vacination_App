1bd771390896d0b9040efc497cd78a6de517291f
joban3152 <jobanjeetsg99@gmail.com>
2021-02-13 00:54:54 -0500

    created child_vaccineList activity

---
+++ app/src/main/java/com/example/vacination_app/child_vaccineList.java
@@ -0,0 +1,14 @@
+package com.example.vacination_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class child_vaccineList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_vaccine_list);
+    }
+}
