586130d9a7ba8005c8d832ba9fa29fa950ae3883
Navandeep Singh <61361120+Navan005@users.noreply.github.com>
2021-02-13 07:14:58 -0500

    children recyclerview

--- app/src/main/java/com/example/vacination_app/AddchildActivity.java
+++ app/src/main/java/com/example/vacination_app/AddchildActivity.java
@@ -4,19 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AddchildActivity extends AppCompatActivity {
 
-    TextView name;
+    private Button addChild;
+    FirebaseFirestore fStore;
+    String userID;
+    EditText child_name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addchild);
-        
 
+        addChild=(Button) findViewById(R.id.btn_addchild);
+        fStore = FirebaseFirestore.getInstance();
+
+        addChild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AddchildActivity.this, ParentshomepageActivity.class);
+                //intent.putExtra("email_key", email);
+                startActivity(intent);
+            }
+        });
     }
 }
