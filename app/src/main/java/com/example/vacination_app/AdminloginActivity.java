915a20a5ccd46cec1b11ff7c22e80990396bc5ed
joban3152 <jobanjeetsg99@gmail.com>
2021-02-13 10:17:26 -0500

    created Admin_dashboard activity

--- app/src/main/java/com/example/vacination_app/AdminloginActivity.java
+++ app/src/main/java/com/example/vacination_app/AdminloginActivity.java
@@ -1,14 +1,29 @@
 package com.example.vacination_app;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class AdminloginActivity extends AppCompatActivity {
+
+    Button Login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminlogin);
+
+        Login_button = (Button) findViewById(R.id.btnLogin);
+
+        Login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminloginActivity.this, Admin_dashboard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
