33e3df344d1753f260789e1297037fb39a59976d
Navandeep Singh <61361120+Navan005@users.noreply.github.com>
2021-02-13 01:55:23 -0500

    error fix

--- app/src/main/java/com/example/vacination_app/MainpageActivity.java
+++ app/src/main/java/com/example/vacination_app/MainpageActivity.java
@@ -20,7 +20,7 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainpageActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
+    
     private Button addChild,addChild2;
 
     @Override
@@ -31,26 +31,15 @@ public class MainpageActivity extends AppCompatActivity {
         addChild=(Button) findViewById(R.id.addRecords);
         addChild2=(Button) findViewById(R.id.addRecords2);
 
-        Intent intent = getIntent();
-        String email = intent.getStringExtra("email_key");
+       // Intent intent = getIntent();
+       // String email = intent.getStringExtra("email_key");
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
-        /*
         addChild.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(MainpageActivity.this, AddchildActivity.class);
-                intent.putExtra("email_key", email);
+                //intent.putExtra("email_key", email);
                 startActivity(intent);
             }
         });
@@ -62,32 +51,9 @@ public class MainpageActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        */
-        
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.mainpage, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
