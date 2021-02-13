e68dcd91a41b1e7d38e4354da326e8ea2ba8538a
joban3152 <jobanjeetsg99@gmail.com>
2021-02-13 00:57:17 -0500

    created recommended_VaccineList_RecyclerView Activity

---
+++ app/src/main/java/com/example/vacination_app/recommended_VaccineList_RecyclerView.java
@@ -0,0 +1,14 @@
+package com.example.vacination_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class recommended_VaccineList_RecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommended__vaccine_list__recycler_view);
+    }
+}
