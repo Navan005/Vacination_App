5da0303a5ecbaa39ed9841c30356000ca01a9ec1
Navandeep Singh <61361120+Navan005@users.noreply.github.com>
2021-02-13 08:56:35 -0500

    children recyclerview

---
+++ app/src/main/java/com/example/vacination_app/addingChild.java
@@ -0,0 +1,14 @@
+package com.example.vacination_app;
+
+public class addingChild {
+
+    private String child_name;
+
+    public addingChild() {
+    }
+
+    public addingChild(String childname){
+
+    }
+
+}
