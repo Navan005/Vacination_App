a06df29e3a5432f27dc67a656201b0b0da481658
joban3152 <jobanjeetsg99@gmail.com>
2021-02-20 00:56:04 -0500

    did commenting in PendingAppointmentActivity activity

--- app/src/main/java/com/example/vacination_app/PendingAppointmentActivity.java
+++ app/src/main/java/com/example/vacination_app/PendingAppointmentActivity.java
@@ -1,14 +1,11 @@
 package com.example.vacination_app;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,7 +15,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//this activity display pending appointments to CLSC
 public class PendingAppointmentActivity extends AppCompatActivity {
 
 
